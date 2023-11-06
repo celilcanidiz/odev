4f54dfee81d40f13a4aed26617fd6fa6bb2b5a5f
celilcanidiz <celilcanidiz@gmail.com>
2023-11-06 19:54:52 +0300

    patika java backend patikası

--- nothesaplama/src/Main.java
+++ nothesaplama/src/Main.java
@@ -3,24 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int matematik, fizik, kimya, turkce, tarih, muzik;
-        double notOrtalamasi;
-        Scanner scanner =new Scanner(System.in);
-        System.out.println("Lütfen matematik notunuzu giriniz.");
-        matematik =scanner.nextInt();
-        System.out.println("Lütfen fizik notunuzu giriniz.");
-        fizik = scanner.nextInt();
-        System.out.println("Lütfen kimya notunuzu giriniz.");
-        kimya=scanner.nextInt();
-        System.out.println("Lütfen türkçe notunuzu giriniz.");
-        turkce=scanner.nextInt();
-        System.out.println("Lütfen tarih notunuzu giriniz.");
-        tarih= scanner.nextInt();
-        System.out.println("Lütfen müzik notunuzu giriniz.");
-        muzik=scanner.nextInt();
+        int mat, fizik, kimya, tarih, muzik, turkce;
 
-        notOrtalamasi=  (matematik + fizik + kimya + turkce + tarih + muzik) /6;
-        System.out.print("Not ortalaması :"+notOrtalamasi);
-        System.out.println(notOrtalamasi < 60 ? " Sınıfta Kaldı" : " Sınıfı Geçti");
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Ders Notunu Griniz .: ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Ders Notunu Griniz .: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Ders Notunu Griniz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Tarih Ders Notunu Griniz .: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Ders Notunu Griniz .: ");
+        muzik = inp.nextInt();
+
+        System.out.print("Türkçe Ders Notunu Griniz .: ");
+        turkce = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + tarih + muzik + turkce);
+        double sonuc = (toplam / 6.0);
+
+        System.out.println("Ders ORTALAMANIZ .: " + sonuc);
+        System.out.println(sonuc < 60 ? " Sınıfta Kaldı" : " Sınıfı Geçti");
     }
 }
