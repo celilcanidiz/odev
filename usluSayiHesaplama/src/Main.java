03778ffad8d89c8b80da064376e7e5ee99c3c6f9
celilcanidiz <celilcanidiz@gmail.com>
2023-11-11 00:48:14 +0300

    üslü sayı hesaplayan program

---
+++ usluSayiHesaplama/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int k,n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü Alınacak Sayi : ");
+        k = input.nextInt();
+        System.out.print("Üs olacak Sayi : ");
+        n = input.nextInt();
+        int total =1;
+
+        for (int i = 1; i <= n; i++){
+            total *= k;
+        }
+        System.out.println("===================");
+        System.out.println("CEVAP : " + total);
+    }
+}
