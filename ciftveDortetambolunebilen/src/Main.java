9e770b631ee9bafbb1e17cc5a80dcbb880d3efd0
celilcanidiz <celilcanidiz@gmail.com>
2023-11-11 00:18:17 +0300

    dörte bölünebilen ve çift olan sayılar toplamı

---
+++ ciftveDortetambolunebilen/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int k;
+        int total = 0;
+        Scanner input = new Scanner(System.in);
+        
+        do {
+            System.out.print("Lütfen Bir Sayı Girin : ");
+            k = input.nextInt();
+            if(k % 4 == 0){
+                total += k;
+            }
+        }while (k % 2 == 0);
+        System.out.println("=============================");
+        System.out.print("\n"+"Çift sayı ve 4'e tam bölünebilen sayıların toplamı: "+ total+"") ;
+    }
+}
