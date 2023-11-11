73f76ee0cdd48559fbff5b77b398ef2f513906d9
celilcanidiz <celilcanidiz@gmail.com>
2023-11-11 16:48:44 +0300

    min max

---
+++ MinMax/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter,sayi,min=0,max=0;
+        System.out.print("Girilecek sayı adedini belirtiniz");
+        counter=input.nextInt();
+        while (counter!=0)
+        {
+            System.out.print("Sayi değerini giriniz");
+            sayi=input.nextInt();
+            if (sayi>max)
+                max=sayi;
+            else if (sayi<min)
+                min=sayi;
+            counter--;
+
+
+        }
+        System.out.println("En büyük: " + max);
+        System.out.println("En küçük" + min);
+
+    }
+}
