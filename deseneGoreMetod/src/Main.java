01d9b89e6a69a5b07d2b5895bb1d18eaee764fd3
celilcanidiz <celilcanidiz@gmail.com>
2023-11-13 17:14:01 +0300

    desene göre metod

---
+++ deseneGoreMetod/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to find pattern: ");
+        int num = input.nextInt();
+        patternCreator(num, num, -1);
+    }
+
+    static void patternCreator(int constantNum, int variableNum, int sign)
+    {
+        if(variableNum > constantNum) return;
+        if(variableNum < 1) sign *= -1;
+        System.out.print(" " + variableNum + " ");
+        patternCreator(constantNum, variableNum  + sign * 5, sign);
+    }
+}
