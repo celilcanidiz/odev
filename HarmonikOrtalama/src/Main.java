d2b2e60e75e6c6a572595978d9e2e990b6ce38dd
celilcanidiz <celilcanidiz@gmail.com>
2023-11-25 12:59:54 +0300

    .

--- HarmonikOrtalama/src/Main.java
+++ HarmonikOrtalama/src/Main.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double result = 0.0;
-        System.out.print("Please Enter the N number: ");
-        int n = input.nextInt();
 
-        for (double i = 1; i <= n; i ++){
-            result += (1/i);
+        double[] numberList = {2,4,6,8};
+        double sum = 0;
+
+        for (double item : numberList) {
+            sum += 1 / item;
         }
-        System.out.println("Harmonic Number is " + result);
+
+        System.out.println(Arrays.toString(numberList) + " Dizisinin");
+        System.out.println("Harmonik Ortalama : " + numberList.length / sum);
     }
 }
