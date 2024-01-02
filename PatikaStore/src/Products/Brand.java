2c424f94d699f3acf07356898af4777274bdc97e
celilcanidiz <celilcanidiz@gmail.com>
2024-01-03 02:14:07 +0300

    PatikaStore

---
+++ PatikaStore/src/Products/Brand.java
@@ -0,0 +1,30 @@
+package Products;
+
+public class Brand implements Comparable<Brand>  {
+    private static int nextId = 1;
+    private final int id;
+    private String name;
+
+    public Brand(String name) {
+        this.id = nextId;
+        nextId++;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Brand o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
---
+++ PatikaStore/src/Products/Notebook.java
@@ -0,0 +1,8 @@
+package Products;
+
+public class Notebook extends Product<Notebook> {
+
+    public Notebook(String brand, String description, double unitPrice, double discount, int stock, int memory, double size, int ram) {
+        super(brand, description, unitPrice, discount, stock,  memory, size, ram);
+    }
+}
